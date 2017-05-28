dd948a4780332f2d113aa4646fa7385680817ca1
Luis Daniel Arevalos <luis.arevalos@my.jcu.edu.au>
2017-05-28 23:27:23 +1000

    Major changes added
    Changes in search:
    - Retrieves all the categories values from de database in the search dropdown
    - search text, search filter, and submit (search button) now is working and is linked with php and jquery functions
    -The page receive a post from index.php page, but it is also capable of receiving a post for itself. Because the initial search could start from the index.php but if the user wants to perform a new search, he can do from the result page without going back to the index.php page again.
    - The resulting data is developed and working, it displays all the searched resources with the name, description, thumbnail if it has it.
    - It also retrieve as a color labels if the resource is free or paid
    - It also shows with colour labels the type of the resource. If is course, book, and so on.
    - It also shows with colour labels if the resource is on site or external.
    - It was added functions that mark with <mark> HTML tag, all the matches with the pattern that was searched. Example if you search PHP, it will search in all the resources if there are any courses, books, and so on, which the same pattern in the name or description, and it will display on the screen the result by marking the pattern found. 
    - It was added a function in which displays the quantity of resources found during the search.

--- e-library/Results.php
+++ e-library/Results.php
@@ -1,4 +1,5 @@
 <?php 
+   
 	include_once('includes/functions.php');
 ?>
 <!DOCTYPE html>
@@ -28,122 +29,73 @@
 					<h1 class="title">Search Results</h1>
 				</div>
 
-				<div class="row" id="searchResult">
-					<div class="col-md-6 col-md-11">
-						<div class="input-group input-group-lg">
-							<div class="input-group-btn">
-								<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All Contents <span class="caret"></span></button>
-								<ul class="dropdown-menu">
-										<li><a href="Categories.html">All Contents</a></li>
-										<li><a href="#">Big Data</a></li>
-										<li><a href="#">Cloud Computing</a></li>
-										<li><a href="#">Enterprise Content Management</a></li>
-										<li><a href="#">GIS</a></li>
-										<li><a href="#">IT and Hardware</a></li>
-										<li><a href="#">Network Administration</a></li>
-										<li><a href="#">Operating System</a></li>
-										<li><a href="#">Security</a></li>
-										<li><a href="#">DBMS</a></li>
-										<li><a href="Programming.html">Programming Languages</a></li>
-										<li><a href="Content.html">Business Intelligence</a></li>                           
-										<li><a href="#">Cloud Computing</a></li>
-										<li><a href="#">Internet Fundamentals</a></li>
-										<li><a href="#">Networking</a></li>
-										<li><a href="#">Security</a></li>
-								</ul>
+				<form method=post action="Results.php">
+					<div class="row" id="searchResult">
+						<div class="col-md-6 col-md-11">
+							<div class="input-group input-group-lg">
+								<div class="input-group-btn">
+									<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All Contents <span class="caret"></span></button>
+									
+									<ul class="dropdown-menu">
+										<li><a href="#">All Contents</a></li>
+										<?php //loading the drop down
+											$conn = db_connect();
+											$sql = oci_parse($conn,'Select CATEGORY_ID, CATEGORY_NAME FROM category');
+											oci_execute($sql);
+											while (($row = oci_fetch_array($sql,OCI_ASSOC+ OCI_RETURN_NULLS))!= false){
+												echo '<li><a href="#">'.$row["CATEGORY_NAME"].'</a></li>';
+											}	
+											oci_close($conn);
+										?>   
+									</ul>
+								</div>
+								<input type="text" class="form-control"  name="search_text" placeholder="Search">
+								<span class="input-group-btn">
+									  <input type="submit" class="btn btn-default" name="submit" value="search" id="btnSearch">
+								</span>
 							</div>
-							<input type="text" class="form-control" placeholder="Search">
-							<span class="input-group-btn">
-								  <button class="btn btn-default" type="button">Search</button>
-							</span>
 						</div>
 					</div>
-				</div>
-				<div class="col-md-6" id="resultQty">
-				 <div class="page-header">
-					<small>About 186,000,000 results</small>
-				</div>
-				</div>
+					<!-- It store the choosen category id -->
+					<input type="hidden"  name="search_category"  id="search_category"> 
+				</form>
+
 			</div>  
 
-			<div class="row">
-				<div class="sixtyPercDiv"> 	
-					<ul class="media-list results">
-						<li class="media">
-							<div class="media-left">
-								<a href="#">
-									<img class="media-object" src="HTMLFundamentals.jpg" alt="...">
-								</a>
-							</div>
-							<div class="media-body">
-								<h4 class="media-heading"><a href="https://www.pluralsight.com/courses/html-fundamentals" target="_blank"><mark>HTML</mark> Fundamentals</a><span class="label label-danger">External Course</span></h4>
-									<mark>HTML</mark> markup is the heart of any web application and this course will teach you the fundamentals of <mark>HTML</mark> regardless of the tool you use to author it. You will learn how to properly define your <mark>HTML</mark> markup and follow the standards, how to create lists, tables, and images, and all about text elements. Along the way you will also be show what NOT to do in your <mark>HTML</mark>.
+			
+				<?php			
+					
+					//to see if the form was submited from another page
+					if (isset($_POST["submit"])){   
+						
+						//boolean_test ? value_if_true : value_if_false
+						$search_text = isset($_POST["search_text"]) ? $_POST["search_text"] : ""; 
+						$category_filter = isset($_POST["search_category"]) ? $_POST["search_category"]:"";
+						
+						$search_text =  strtoupper($search_text);
+						$category_filter= strtoupper($category_filter);
+						
+						//make the database connection
+						$conn = db_connect();
+						
+						$sql_str = "SELECT RESOURCE_NAME, RESOURCE_SHORT_DSC, RESOURCE_THUMBNAIL , RESOURCE_ACCESS, SOURCE,TYPE_DSC,count(*) over (partition by 1) QTY
+									FROM RESOURCES r, CATEGORY c , TYPE T
+									WHERE c.category_id = r.category_id
+									and r.type_id = t.type_id" ;
+						if ($search_text != ""){
+							$sql_str = $sql_str . " and UPPER(RESOURCE_NAME) like '%$search_text%'";
 
-							</div>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<div class="row">
-				<div class="sixtyPercDiv"> 	
-					<ul class="media-list results">
-						<li class="media">
-							<div class="media-left">
-								<a href="#">
-									<img class="media-object" src="HTMLDreamviewer.jpg" alt="">
-								</a>
-							</div>
-
-							<div class="media-body">
-								<h4 class="media-heading"><a href="https://helpx.adobe.com/dreamweaver/how-to/work-with-html-layout.html" target="_blank"><mark>HTML</mark> With Dreamweaver</a><span class="label label-danger">External Course</span></h4>
-									A well-defined web page structure provides the foundation for your layout and styling. See how to use Hypertext Markup Language <mark>(HTML)</mark> to structure pages in Adobe Dreamweaver.
-
-							</div>
-						</li>
-					</ul>
-				</div>	
-			</div>
-			<div class="row">
-				<div class="sixtyPercDiv"> 	
-					<ul class="media-list results">
-						<li class="media">
-							<div class="media-left">
-								<a href="#">
-									<img class="media-object" src="HTMLBook.jpg" alt="">
-								</a>
-							</div>
-
-							<div class="media-body">
-								<h4 class="media-heading"><a href="https://www.amazon.com.au/HTML-Web-Guide-Absolute-Beginners-Development-Book/dp/B015JNEOZY/ref=sr_1_10?ie=UTF8&qid=1491917135&sr=8-10&keywords=html+book" target="_blank"><mark>HTML</mark>: Web Guide For Absolute <mark>HTML</mark></a><span class="label label-warning">Paid Book</span></h4>
-									<mark>HTML</mark> Web Guide For Absolute Beginners is a book for beginners. No prior knowledge of <mark>HTML</mark> required. You don't need to read a 500 pages book to learn <mark>HTML</mark>. Just follow these simple examples and become proficient with <mark>HTML</mark> in less than 24 hours. Straightforward examples. So easy, anyone can learn it!
-
-
-							</div>
-
-						</li>
-					</ul>
-				</div>	
-			</div>
-
-			<div class="row">
-				<div class="sixtyPercDiv"> 	
-					<ul class="media-list results">
-						<li class="media">
-							<div class="media-left">
-								<a href="#">
-									<img class="media-object" src=" " alt=" ">
-								</a>
-							</div>
-
-							<div class="media-body">
-								<h4 class="media-heading"><a href="https://www.amazon.com.au/HTML-Web-Guide-Absolute-Beginners-Development-Book/dp/B015JNEOZY/ref=sr_1_10?ie=UTF8&qid=1491917135&sr=8-10&keywords=html+book" target="_blank"><mark>HTML</mark>: Inside Course Example <mark>HTML</mark></a><span class="label label-success">Free Course</span><span class="label label-info">Inside Course</span></h4>
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam repellat, maiores quaerat, officia quo quidem dolores possimus nostrum quasi nesciunt, dignissimos pariatur necessitatibus eos dolor. Numquam, ullam veritatis nesciunt ea!
-
-							</div>										
-						</li>
-					</ul>
-				</div>		
-			</div>
+						} 
+						if ($category_filter != "" and $category_filter != 'All Contents'){
+							$sql_str = $sql_str . " and UPPER(CATEGORY_NAME) = '$category_filter'";
+						}
+						
+						$sql = oci_parse($conn,$sql_str);
+						oci_execute($sql);
+						
+						display_results($conn, $sql,$search_text);						
+					}				
+				?>		
 
 			<nav aria-label="...">
 				<ul class="pagination" id="marginPagination">
