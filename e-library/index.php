7b3c784089c20fc22858be129bd3b77413facc33
Luis Daniel Arevalos <luis.arevalos@my.jcu.edu.au>
2017-05-28 23:36:50 +1000

    Major changes added in index.
    Changes in search:
    - Retrieves all the categories values from de database in the search dropdown
    - It was added id and names in some tags that work with jquery and php to make the search and the filter works
    - It was added all the logic in search, it make a post and call the result.php page in order to show the results.
    
    Hot Categories:
    - Now the tab hot categories really recover the 'Hot categories'. It is linked to a php function in which it recover from the database the top 6 most viewed categories.
    
    New Topics:
    - The new topic tab now really shows the last 6 topic added in the database.

--- e-library/index.php
+++ e-library/index.php
@@ -12,17 +12,17 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="css/bootstrap.css" rel="stylesheet">
 		<link href="css/e_library.css" rel="stylesheet" type="text/css">
-		<!--<script>
-			function nav_category_id_selected(cat_id){
-				
-			} 
-		</script>-->
 	</head>
   
 	<body style="padding-top: 50px">
+	<!--<p id="search_category"> </p>-->
+	 
+		<div id="script">
 		<?php 
 			echo navigation_bar();
+			
 		?>
+		</div>
 		<div class="container-fluid">
 			<div class="row" id="principalImage">
 				<form method=post action="Results.php">
@@ -32,6 +32,7 @@
 								<div class="input-group-btn">
 									<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All Contents <span class="caret"></span></button>
 									<ul class="dropdown-menu">
+										<li><a href="#">All Contents</a></li>
 										<?php //loading the drop down
 											$conn = db_connect();
 											$sql = oci_parse($conn,'Select CATEGORY_ID, CATEGORY_NAME FROM category');
@@ -45,90 +46,35 @@
 								</div>						
 								<input type="text" class="form-control"  name="search_text" placeholder="Search">
 								<span class="input-group-btn">
-								<input type="submit" class="btn btn-default" name="submit" value="submit">
-													
+									<input type="submit" class="btn btn-default" name="submit" value="Search" id="btnSearch">
+									
+									<!-- <input type="text" class="form-control"  name="search_category" placeholder="Search" id="search_category"> -->
+									
 								</span>					
 							</div>
 						 </div>
 					</div>
+   	 				<!-- It store the choosen category id -->
+    	 			<input type="hidden"  name="search_category"  id="search_category"> 
      	 		</form>
 	     	 	<h1>Welcome to IT-Library</h1>
 		     	<h3>All IT sources in one site. Books, tutorials, apps and much more.</h3>
 			 	<img src="images/library.png" class="img-responsive blurImage" alt="E-library Image Banner">		
 			</div>
 		  	<ul class="nav nav-tabs" id="tabs">
-			  <li role="presentation" class="active"><a href="index.html">Hot Topics</a></li>
-			  <li role="presentation"><a href="index_newTopic.html">New Topics</a></li>  
+			  <li role="presentation" class="active"><a href="index.php">Hot Categories</a></li>
+			  <li role="presentation"><a href="index_newTopic.php">New Topics</a></li>  
 			</ul>			
 			
-			<section id="Section_Hot-topics" class="row">		  
-				<div class="col-lg-10 col-lg-offset-1">
-					<div class="row">
-						<article class="col-sm-6 col-md-4 col-lg-4 ">
-							<div class="thumbnail">
-								<a href="Content.html"><h3>Business Intelligence</h3></a>
-								<div class="caption">
-									
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima tempora aliquam quod eaque, dicta, sed autem quasi est architecto, accusantium temporibus! Fugit quos, autem voluptates nam laudantium asperiores optio earum.</p>
-									<p> <a href="#" class="btn btn-default" role="button">Explore &gt&gt</a></p>
-								</div>
-							</div>
-						</article>
-					
-						<article class="col-sm-6 col-md-4 col-lg-4">
-							<div class="thumbnail">
-								<h3 >Big Data</h3>
-								<div class="caption">
-									
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi alias exercitationem perferendis, eos, debitis, pariatur reiciendis saepe veritatis eligendi inventore illum odio repudiandae, atque id tenetur ex consequatur porro sunt.</p>
-									<p> <a href="#" class="btn btn-default" role="button">Explore &gt&gt</a></p>
-								</div>
-							</div>
-						</article>
-						<article class="col-sm-6 col-md-4 col-lg-4">
-							<div class="thumbnail">
-								<h3>DBMS</h3>
-								<div class="caption">
-									
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum distinctio nam qui voluptate, laudantium veritatis animi dignissimos facilis eius incidunt minima magnam enim voluptatibus optio placeat voluptates id ducimus dicta.</p>
-									<p> <a href="#" class="btn btn-default" role="button">Explore &gt&gt</a></p>
-								</div>
-							</div>
-						</article>
-						<article class="col-sm-6 col-md-4 col-lg-4">
-							<div class="thumbnail">
-								<h3 >Security</h3>
-								<div class="caption">
-									
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, labore, eaque? Nesciunt corporis necessitatibus, voluptatibus magni aliquam in quibusdam perferendis natus optio quos voluptate neque, esse recusandae ducimus repudiandae. Aperiam.</p>
-									<p><a href="#" class="btn btn-default" role="button">Explore &gt&gt</a></p>
-								</div>
-							</div>
-						</article>
-						<article class="col-sm-6 col-md-4 col-lg-4">
-							<div class="thumbnail">
-								<h3>Virtualization</h3>
-								<div class="caption">
-									
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolore aliquam dolorem harum ab adipisci dignissimos officia tempora ipsa deserunt ex eveniet cumque sit, quia corporis, blanditiis hic voluptate enim?</p>
-									<p> <a href="#" class="btn btn-default" role="button">Explore &gt&gt</a></p>
-								</div>
-							</div>
-						</article>
-						<article class="col-sm-6 col-md-4 col-lg-4">
-							<div class="thumbnail">
-								<h3 >Cloud Computing</h3>
-								<div class="caption">
+
+			<?php 
+				//it will dfesplay the 6 most viewed categories
+				display_hot_categories();
 							
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem obcaecati eaque eligendi rem quod rerum quam provident, dolor labore, unde dignissimos consequuntur, nulla alias, illo inventore illum vero. Minima, incidunt.</p>
-									<p> <a href="#" class="btn btn-default" role="button">Explore &gt&gt</a></p>
-								</div>
-							</div>
-						</article>
-					</div>
-				</div>
-			</section>
-			<?php footer() ?>
+
+				//it will display de footer
+				footer();
+			?>
 		</div>
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
 		<script src="js/jquery-1.11.3.min.js"></script>
