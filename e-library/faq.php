b754ea5e5a27e1537cf6b2b719486f103540488d
ajinkyagagare <ajinkyabalasaheb.gagare@my.jcu.edu.au>
2017-05-30 15:49:27 +1000

    New page
    New FAQs page added

---
+++ e-library/faq.php
@@ -0,0 +1,120 @@
+<?php 
+	include_once('includes/functions.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+	 <head>
+		<meta charset="utf-8">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Welcome to E-library</title>
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/e_library.css" rel="stylesheet" type="text/css">
+	</head>
+  
+	<body style="padding-top: 50px">
+		<?php 
+			echo navigation_bar();
+			//echo $value;
+		?>
+ <ol class="breadcrumb">
+  <li><a href="index.html">Home</a></li>
+  <li><a href="#">FAQ</a></li>
+ 
+</ol>
+
+    <div class="container-fluid">
+		<div class="row">
+
+			                                      <!-- Code for FAQ (Start) -->
+<br>
+<h1 id="faq" style="text-align:center;" ><strong>FAQs</strong></h1>
+
+
+
+<h1 style="text-align:center;">How can we help?</h1>
+<p style="text-align:center;">Not sure where to start your research? Stuck somewhere along the way? Ask a Librarian.
+Our librarians provide research tips and strategies to point you in the right direction.</p>
+
+<br>
+<h1 style="text-align:center;">What do I do if I get an error message while trying to access an e-library online resource?</h1>
+<p style="text-align:center;">It is advisable to try again later, as some databases are licensed for only a certain number of concurrent users. If the error persists please contact library staff.</p>
+
+
+<br>
+<h1 style="text-align:center;">Can I access the e-library resources from home?</h1>
+<p style="text-align:center;">Yes, most e-resources are available for access from home or anywhere with an internet connection. Ancestry & Find My Past can only be used at the library branches.</p>
+<br>
+
+
+<br>
+<h1 style="text-align:center;">What do the online eLibrary resources contain?, expanded</h1>
+<p style="text-align:center;">The online e-resources contain e-books, e-audio books, downloadable music, online magazines, current newspaper articles from the world's leading newspapers; articles from editions of popular magazines; genealogy resources via newspaper databases and the Ancestry database; legal aid assistance; employment assistance and much more. See the e-library for more details.</p>
+<br>
+
+<br>
+<h1 style="text-align:center;">I am having trouble downloading an e-book/audio book ..., collapsed</h1>
+<p style="text-align:center;">Overdrive and Bolinda websites have excellent help screens. For problems with Overdrive, visit this link.  For problems with Bolinda, visit this link then log-in to access the help screen.</p>
+<br>
+
+<br>
+<h1 style="text-align:center;">Can I download an e-book on a library PC?, expanded</h1>
+<p style="text-align:center;">Titles can be borrowed using a library PC but must be downloaded/synced to a device. The Overdrive Download Station located at Mill Park library allows patrons to download directly to that library PC.
+There are some titles in the Overdrive collection called Overdrive Read which can be read on a PC (including a library PC). Remember to adhere to PC booking times, if you wish to read a title in the library.</p>
+<br>
+
+<br>
+<h1 style="text-align:center;"></h1>
+<p style="text-align:center;"> </p>
+<br>
+
+
+
+
+<br>
+<h1 style="text-align:center;">How can i Visit?</h1>
+<p style="text-align:center;">Speak to a librarian in one of our reading rooms.
+
+Or Book a Librarian to make an appointment with a librarian or curator for professional guidance about your research.</p>
+<br>
+<h1 style="text-align:center;">What is the response time?</h1>
+<p style="text-align:center;">We can spend up to one hour on enquiries. Most are answered within a few days, however complex and special collection enquiries may take one week or longer.</p>
+<br>
+<br>
+
+
+
+		 
+
+
+
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br
+   /><br /><br /><br />
+   
+
+
+
+
+
+
+
+
+                                              <!-- Code for FAQ (End) -->  
+		
+	
+		
+		<?php 
+
+
+				//it will display de footer
+				footer();
+			?>
+		</div>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+		<script src="js/jquery-1.11.3.min.js"></script>
+		<script src="js/bootstrap.js"></script>
+		</div>
+	</body>
+</html>
