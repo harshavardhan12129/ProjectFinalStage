7278b9ff6bc697cebc2a8526f5f5d2c862bbeddd
Luis Daniel Arevalos <luis.arevalos@my.jcu.edu.au>
2017-05-31 00:34:01 +1000

    Updated dynamic navigation bar and footer

--- e-library/logout_oracle.php
+++ e-library/logout_oracle.php
@@ -1,3 +1,6 @@
+<?php 
+	include_once('includes/functions.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 	 <head>
@@ -12,47 +15,11 @@
 	</head>
   
 	<body style="padding-top: 50px">
-    <nav class="navbar navbar-default navbar-fixed-top">	
-			<div class="container-fluid"> 
-				<div class="navbar-header">
-					<button type="button"  class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topFixedNavbar1" aria-expanded="false"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-					<a class="navbar-brand" href="index.html">IT-Library</a>
-				</div>
-				<div class="collapse navbar-collapse" id="topFixedNavbar1">
-					<ul class="nav navbar-nav">
-					  <li class="active"><a href="index.html">Home<span class="sr-only">(current)</span></a></li>
-					  <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categories<span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="Categories.html">Select from all</a></li>
-							<li><a href="#">Big Data</a></li>
-							<li><a href="#">Cloud Computing</a></li>
-							<li><a href="#">Enterprise Content Management</a></li>
-							<li><a href="#">GIS</a></li>
-							<li><a href="#">IT and Hardware</a></li>
-							<li><a href="#">Network Administration</a></li>
-							<li><a href="#">Operating System</a></li>
-							<li><a href="#">Security</a></li>
-							<li><a href="#">DBMS</a></li>
-							<li><a href="Programming.html">Programming Languages</a></li>
-							<li><a href="Content.html">Business Intelligence</a></li>                           
-							<li><a href="#">Cloud Computing</a></li>
-							<li><a href="#">Internet Fundamentals</a></li>
-							<li><a href="#">Networking</a></li>
-							<li><a href="#">Security</a></li>                 
-						</ul>
-					  </li>
-					</ul>
-					<div class="btn-toolbar" role="toolbar" id="btn-toolbar-login-signup">
-						<div class="btn-group" role="group">
-							<a href="Login.html" type="button" class="btn btn-default">Login</a>
-						</div>
-						<div class="btn-group" role="group">
-						  <a href="SignUp.html"  type="button" class="btn btn-primary">Sign Up</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</nav>
+	<?php 
+	
+			echo navigation_bar();
+			
+		?>
    
     <div class="container-fluid">
 		<div class="row">
@@ -60,7 +27,7 @@
   <li><a href="index.html">Home</a></li>
   <li><a href="#">Login</a></li>
 <?php
-  session_start();
+  
   if(isset($_SESSION['valid_user']))
   {
   	$old_user = $_SESSION['valid_user'];  // store  to test if user *was* logged in
@@ -98,104 +65,9 @@
   echo '<a href="login_oracle.php">Back to login page</a>';
   echo '</body>';
   echo '</html>';
-?> 
-<div class="row">
-				
-				<footer class="col-md-12" >
-			   		<div class="row" id="row1FooterContent" >
-			      		
-			      		<div class="col-md-2 footer-links">
-			     			<h4><strong>Quick Links</strong> </h4>
-			     		 </div>
-			     		 <div class="col-md-2  footer-links">
-			     		 	<h4><strong>About us</strong></h4>
-			     		 	<a href="AboutUs">About Us</a>
-			     		 	<a href="#">Subscribe as a teacher</a>
-			     		 </div>
-			      		<div class="col-md-2 footer-links">
-			      			<h4><strong>Support</strong></h4>
-			      			<a href="#">Support</a>
-			      			<a href="#">Help</a>
-			      			<a href="ContactUs.html">Contact Us</a>
-			      			<a href="#">Give us Feedback</a>
+	footer();
+		?> 
 
-			      		</div>
-			     		 
-			     		 <div class="col-md-2 footer-links">
-							 <h4><strong>Social Networking</strong></h4>
-                             <!-- Add font awesome icons -->
-					<a href="#" class="fa fa-facebook"></a>	
-					<a href="#" class="fa fa-twitter"></a>
-					<a href="#" class="fa fa-google"></a>
-					<a href="#" class="fa fa-linkedin"></a>
-					<a href="#" class="fa fa-youtube"></a>
-					<a href="#" class="fa fa-instagram"></a>
-                
-                    <style>
-.fa {
-   padding: 20px;
-  font-size: 15px;
-  width: 50px;
-  text-align: center;
-  text-decoration: none;
-  margin: 5px 2px;
-  
-}
-
-.fa:hover {
-    opacity: 0.7;
-}
-
-.fa-facebook {
-  background: #3B5998;
-  color: white;
-}
-
-
-.fa-twitter {
-  background: #55ACEE;
-  color: white;
-}
-
-.fa-google {
-  background: #dd4b39;
-  color: white;
-}
-
-.fa-linkedin {
-  background: #007bb5;
-  color: white;
-}
-
-.fa-youtube {
-  background: #bb0000;
-  color: white;
-}
-
-.fa-instagram {
-  background: #125688;
-  color: white;
-}
- </style>
-			     		 </div>
-			     		
-			     		 
-		       		</div>
-		       		<div class="col-xs-12"><hr></div>
-		       		<div id="site-links" class="container">
-						<div class="row">
-							<div class="col-sm-7 col-lg-9">
-								<ul class="footer-IT-Library">
-									<li id="copyRight">© 2017 IT Library group</li>
-										<li><a class="ga" href="sitemap.html" data-ga-category="footer" data-ga-action="click" data-ga-label="sitemap" data-qa="link-site-map">Site Map</a></li>
-									<li><a class="ga" href="#" data-ga-category="fat-footer" data-ga-action="click" data-ga-label="privacy-policy" data-qa="link-privacy-policy">Privacy policy</a></li>
-									<li><a class="ga" href="#" data-ga-category="footer" data-ga-action="click" data-ga-label="web-use-policy" data-qa="link-website-use">Web Use Policy</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-			  	</footer>			
-			</div>
 		</div>
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
 		<script src="js/jquery-1.11.3.min.js"></script>
